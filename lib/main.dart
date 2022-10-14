998c883a595b8bc6f90c6b26d6c620cc0ea180c8
Raad <yamin.raad6109@gmail.com>
2022-10-14 17:39:51 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Vangti Chai'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    ),
+  );
+}
